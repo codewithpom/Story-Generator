b25c34fe2ffed18a0814fad989a6ab02b9902db8
Padmashree Jha <padmashreejha717@gmail.com>
2021-03-24 15:24:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+when = ['A few years ago', 'Yesterday', 'Last night', 'A long time ago','On 20th Jan']
+who = ['a rabbit', 'an elephant', 'a mouse', 'a turtle','a cat']
+went = ['cinema', 'university','seminar', 'school', 'laundry']
+happened = ['made a lot of friends','Eats a burger', 'found a secret key', 'solved a mistery', 'wrote a book']
+
+print(random.choice(when) + " " + random.choice(who) + " went to " + random.choice(went) + " and " + random.choice(happened))
